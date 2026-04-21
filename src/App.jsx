46a162c81b29fac8a1bdf2df746b118b054ed7e7
kenzy-123 <kinzem68@gmail.com>
2026-04-21 21:16:12 +0200

    test2

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,14 @@
 
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
-import AcceptableTalent from "./pages/AcceptableTalent/AcceptableTalent.jsx";
-import SuccessStoriesMo from "./pages/SuccessStoriesMo/SuccessStoriesMo.jsx";
-import SuccessStoriesCr from "./pages/SuccessStoriesCr/SuccessStoriesCr.jsx";
-import SuccessStoriesLeo from "./pages/SuccessStoriesLeo/SuccessStoriesLeo.jsx";
-import Blog from "./pages/Blog/Blog.jsx";
-import Header from "./components/Header/Header.jsx";
 
 
 const App = () => {
   return (
     <div>
       <Header />
-  <Routes>
+    <Routes>
     <Route path="/" element={<Home />} />
-    <Route path="/AcceptableTalent" element={<AcceptableTalent />} />
-    <Route path="/SuccessStoriesMo" element={<SuccessStoriesMo />} />
-    <Route path="/SuccessStoriesCr" element={<SuccessStoriesCr />} />
-    <Route path="/SuccessStoriesLeo" element={<SuccessStoriesLeo />} />
-    <Route path="/Blog" element={<Blog />} />
   </Routes>
   </div>
   )
