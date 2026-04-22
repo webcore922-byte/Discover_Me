8811a2c5936e739530a9b8a1e75cae10ca4bb8c0
ahmed-mo-001 <ahmed.mo.agour@gmail.com>
2026-04-23 00:42:19 +0200

    propernutrition push

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home.jsx";
 import AcceptableTalent from "./pages/AcceptableTalent/AcceptableTalent.jsx";
 import SuccessStoriesMo from "./pages/SuccessStoriesMo/SuccessStoriesMo.jsx";
 import SuccessStoriesCr from "./pages/SuccessStoriesCr/SuccessStoriesCr.jsx";
 import SuccessStoriesLeo from "./pages/SuccessStoriesLeo/SuccessStoriesLeo.jsx";
-import Blog from "./pages/Blog/Blog.jsx";
-
+import ProperNutrition from "./pages/Blog/ProperNutrition.jsx";
 
 const App = () => {
   return (
     <div>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/AcceptableTalent" element={<AcceptableTalent />} />
-    <Route path="/SuccessStoriesMo" element={<SuccessStoriesMo />} />
-    <Route path="/SuccessStoriesCr" element={<SuccessStoriesCr />} />
-    <Route path="/SuccessStoriesLeo" element={<SuccessStoriesLeo />} />
-    <Route path="/Blog" element={<Blog />} />
-  </Routes>
-  </div>
-  )
-}
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/AcceptableTalent" element={<AcceptableTalent />} />
+        <Route path="/SuccessStoriesMo" element={<SuccessStoriesMo />} />
+        <Route path="/SuccessStoriesCr" element={<SuccessStoriesCr />} />
+        <Route path="/SuccessStoriesLeo" element={<SuccessStoriesLeo />} />
+        <Route path="/ProperNutrition" element={<ProperNutrition />} />
+      </Routes>
+      <ProperNutrition />
+    </div>
+  );
+};
 
-export default App
+export default App;
