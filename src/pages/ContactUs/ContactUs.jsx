7908156db15ab5742ff3c6b74b9fd516ed53e538
ahmed-mo-001 <ahmed.mo.agour@gmail.com>
2026-05-05 20:22:32 +0300

    amdtest

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,12 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const ContactUs = () => {
-  return (
-    <div>
-      <h1>Contact Us</h1>
-      
-    </div>
-  )
-}
+  return <div>ContactUs</div>;
+};
 
-export default ContactUs
+export default ContactUs;
