dbbb100be8230e7b92fb05a77b4c949c0f4aae23
kenzy-123 <kinzem68@gmail.com>
2026-06-30 01:17:33 +0300

    ki

--- src/components/Dashboard/ContestsSection.jsx
+++ src/components/Dashboard/ContestsSection.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Save, X, Gift, Calendar, Award, MapPin, Target, List, Trophy, Ban, Crown, User, Check } from 'lucide-react';
-import UseContestsLogic from '../../../src/components/Dashboard/UsecontestsLogic.jsx';
+import UseContestsLogic from './UsecontestsLogic.jsx';
 
 const ContestsSection = () => {
   const navigate = useNavigate();
