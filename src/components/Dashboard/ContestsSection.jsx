1b994c4168828458ef2cc68fcf5c84e90720b1c9
kenzy-123 <kinzem68@gmail.com>
2026-06-29 22:47:30 +0300

    fix: add missing ContestsLogic file

--- src/components/Dashboard/ContestsSection.jsx
+++ src/components/Dashboard/ContestsSection.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Save, X, Gift, Calendar, Award, MapPin, Target, List, Trophy, Ban, Crown, User, Check } from 'lucide-react';
-import UseContestsLogic from './UseContestsLogic';
+import UseContestsLogic from '../../../src/components/Dashboard/Usecontestslogic.jsx';
 
 const ContestsSection = () => {
   const navigate = useNavigate();
